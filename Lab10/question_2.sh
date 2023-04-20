f42d140baad11970e1e00dba7f236ea6d4947651
MichaelR13 <michaels.rojas13@csu.fullerton.edu>
2023-04-20 12:57:14 -0700

    Renamed scripts for lab

--- Lab10/question_2.sh
+++ Lab10/question_2.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+printf "\n[some intro info]" 
+printf "\n[key entering guidance]"  
+printf "\n0 Exit" 
+printf "\n1 Project Introduction" 
+printf "\n2 File Integrity Check Helper" 
+printf "\n3 Dependency Check Helper" 
+printf "\n4 Credits"
+printf "\n…..."
